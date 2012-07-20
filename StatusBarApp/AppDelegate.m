bd0a6994855b4d23ab10142b54034f03d6fdbdcf
lepture <lepture@me.com>
2012-07-20 20:32:07 +0800

    make it work

--- StatusBarApp/AppDelegate.m
+++ StatusBarApp/AppDelegate.m
@@ -19,4 +19,16 @@
     // Insert code here to initialize your application
 }
 
+- (void) awakeFromNib {
+    self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    
+    self.statusBar.title = @"G";
+    
+    // you can also set an image
+    //self.statusBar.image = 
+    
+    self.statusBar.menu = self.statusMenu;
+    self.statusBar.highlightMode = YES;
+}
+
 @end
